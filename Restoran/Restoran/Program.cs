a45d0f8774ac29442e779eea12a57dda50053250
XANDRO <XANDRO@DESKTOP-BJKH4CP>
2020-06-26 01:22:32 +0200

    Changes to code

---
+++ Restoran/Restoran/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Restoran
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
